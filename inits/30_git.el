6e1fe6bbe5cadcd5b99304171ffc2bafe3b7c92c
Yadex205 <yadex205@outlook.jp>
2016-07-06 15:38:26 +0900

    :+1: add keybinds for magit

--- inits/30_git.el
+++ inits/30_git.el
@@ -7,8 +7,14 @@
 (require 'magit)
 
 ;; Set keybinds for magit
-(define-key global-map (kbd "C-c C-m s") 'magit-status)
-(define-key global-map (kbd "C-c C-m l") 'magit-log)
-(define-key global-map (kbd "C-c C-m p") 'magit-push)
+(define-key global-map (kbd "C-c C-m C-s") 'magit-status)
+(define-key global-map (kbd "C-c C-m C-l") 'magit-log)
+(define-key global-map (kbd "C-c C-m C-p") 'magit-push)
+(define-key global-map (kbd "C-c C-m C-c") 'magit-checkout)
+(define-key global-map (kbd "C-c C-m C-m") 'magit-merge)
+(define-key global-map (kbd "C-c C-m C-d") 'magit-diff)
+(define-key global-map (kbd "C-c C-m r") 'magit-file-rename)
+(define-key global-map (kbd "C-c C-m C-r") 'magit-revert)
+(define-key global-map (kbd "C-c C-m M-r") 'magit-rebase)
 
 ;;; 30_git.el ends here
