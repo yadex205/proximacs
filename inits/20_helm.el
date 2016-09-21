1c21cfd71ec0ac225782b66e8c5cd6f6a0684f70
Yadex205 <yadex205@outlook.jp>
2016-09-21 14:09:34 +0900

    :memo: Add comment

--- inits/20_helm.el
+++ inits/20_helm.el
@@ -17,8 +17,10 @@
                  ;; Disable helm auto update
                  (setq helm-ff-auto-update-initial-value nil)
 
-                 ;; Unknown
+                 ;; Erase after cursor when C-k inputted
                  (setq helm-delete-minibuffer-contents-from-point t)
+
+                 ;; Unknown
                  (delete '(find-file) helm-completing-read-handlers-alist)
                  (delete '(execute-extended-command) helm-completing-read-handlers-alist)
                  ))
