d5e60d3580ff194ac5dc08ebd051d3e095da2e29
Kanon Kakuno <yadex205@outlook.jp>
2017-10-20 14:41:14 +0900

    :wrench: change C language standard to C++11

---
+++ inits/99_clang.el
@@ -0,0 +1,13 @@
+;;; 99_clang.el --- My init script to configure C/C++ coding environment
+
+
+;;; Commentary:
+
+
+;;; Code:
+
+(custom-set-variables
+ `(flycheck-clang-language-standard "c++11"))
+
+
+;;; 99_clang.el ends here
