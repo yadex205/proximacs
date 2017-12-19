c04484ec02b77846e3489ed69a98c261eeae60a4
Kanon Kakuno <yadex205@outlook.jp>
2017-12-19 13:41:13 +0900

    :+1: load web-mode on erb file

--- inits/99_web_js.el
+++ inits/99_web_js.el
@@ -12,6 +12,7 @@
 (add-to-list 'auto-mode-alist '("\\.htm$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.ejs$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.php$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.jake$" . js2-mode))
