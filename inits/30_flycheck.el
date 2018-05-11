0fbebf0114836208a0834be502e30ed84bfb706a
Kanon Kakuno <kanon.kakuno@4digit.jp>
2018-05-11 10:36:26 +0900

    :+1: Shorten delay until flycheck error messages displayed

--- inits/30_flycheck.el
+++ inits/30_flycheck.el
@@ -9,7 +9,10 @@
 (global-flycheck-mode 1)
 (flycheck-status-emoji-mode t)
 (flycheck-popup-tip-mode t)
-
 (flycheck-cask-setup)
+
+(custom-set-variables
+ '(flycheck-popup-tip-error-prefix "😱 ")
+ '(flycheck-display-errors-delay 0.3))
 
 ;;; 30_flycheck.el ends here
