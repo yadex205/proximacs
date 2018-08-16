e8da2eeec314b29fdce3ab05b02ecf49b557fed6
Kanon Kakuno <kanon.kakuno@4digit.jp>
2018-08-16 14:34:53 +0900

    :+1: Temporary disable whitespace when company-mode is active.

--- inits/40_company_mode.el
+++ inits/40_company_mode.el
@@ -5,6 +5,7 @@
 ;;; Code:
 
 (require 'company)
+(require 'whitespace)
 
 (global-company-mode)
 
@@ -12,6 +13,19 @@
 (define-key company-active-map (kbd "C-p") 'company-select-previous)
 (define-key company-search-map (kbd "C-n") 'company-select-next)
 (define-key company-search-map (kbd "C-p") 'company-select-previous)
+
+(add-hook 'company-completion-started-hook
+          (lambda (started-manually)
+            (whitespace-mode -1)))
+
+(add-hook 'company-completion-cancelled-hook
+          (lambda (cancelled-manually)
+            (whitespace-mode t)))
+
+(add-hook 'company-completion-finished-hook
+          (lambda (selected-candidate)
+            (whitespace-mode t)))
+
 
 (custom-set-variables
  '(company-idle-delay 0))
