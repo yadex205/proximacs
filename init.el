0823e7fdd92838ec2adea3d250d02a4eab554612
Kanon Kakuno <yadex205@outlook.jp>
2019-09-15 10:41:04 +0700

    :sparkles: chore(straight.el) Replace Quelpa with straight.el

--- init.el
+++ init.el
@@ -38,13 +38,90 @@
 (setq initial-scratch-message "")
 (menu-bar-mode 0)
 
-(add-to-list 'load-path "~/.emacs.d/depends/quelpa")
-(custom-set-variables
- '(quelpa-update-melpa-p nil))
-(require 'quelpa)
+;; straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-(load "~/.emacs.d/quelpa-packages")
+;; init-file management
+(straight-use-package 'init-loader)
 
+;; appearance
+(straight-use-package 'monokai-theme)
+(straight-use-package 'nlinum)
+(straight-use-package 'path-headerline-mode)
+(straight-use-package 'rainbow-mode)
+(straight-use-package 'rainbow-delimiters)
+(straight-use-package 'powerline)
+(straight-use-package 'shackle)
+
+;; utils
+(straight-use-package 'multi-term)
+(straight-use-package 'dired-hacks)
+(straight-use-package 'magit)
+
+;; editor enhancement
+(straight-use-package 'anzu)
+(straight-use-package 'company)
+(straight-use-package 'flycheck)
+(straight-use-package 'flycheck-status-emoji)
+(straight-use-package 'flycheck-popup-tip)
+(straight-use-package 'helm)
+(straight-use-package 'helm-ag)
+(straight-use-package 'helm-ls-git)
+(straight-use-package 'helm-flycheck)
+(straight-use-package 'mwim)
+
+;; C/C++
+(straight-use-package 'irony)
+(straight-use-package 'irony-eldoc)
+(straight-use-package 'company-irony)
+(straight-use-package 'flycheck-irony)
+
+;; Objective C
+(straight-use-package 'flycheck-objc-clang)
+
+;; Ruby
+(straight-use-package 'rspec-mode)
+(straight-use-package 'ruby-end)
+(straight-use-package 'yard-mode)
+
+;; Frontend
+(straight-use-package 'web-mode)
+(straight-use-package 'sass-mode)
+(straight-use-package 'scss-mode)
+(straight-use-package 'stylus-mode)
+(straight-use-package 'emmet-mode)
+(straight-use-package 'company-web)
+(straight-use-package 'js2-mode)
+(straight-use-package 'typescript-mode)
+(straight-use-package 'tide)
+(straight-use-package 'vue-mode)
+
+;; Java
+(straight-use-package 'kotlin-mode)
+(straight-use-package 'groovy-mode)
+
+;; languages
+(straight-use-package 'cmake-mode)
+(straight-use-package 'glsl-mode)
+(straight-use-package 'graphviz-dot-mode)
+(straight-use-package 'gitignore-mode)
+(straight-use-package 'json-mode)
+(straight-use-package 'markdown-mode)
+(straight-use-package 'nginx-mode)
+(straight-use-package 'php-mode)
+(straight-use-package 'yaml-mode)
+;
 ;; Load external init scripts
 (require 'init-loader)
 (setq init-loader-show-log-after-init t)
